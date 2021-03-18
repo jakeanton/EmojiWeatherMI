da8d5def96f494a4ba81d92cc19c97da613873b7
Jake Schreuder <jakeanton@gmail.com>
2021-03-18 18:15:59 -0400

    Adding a static emoji map of Michigan. Put in some basic weather setup with OpenWeatherMaps. Removing venv from source control.

--- main.py
+++ main.py
@@ -1,23 +1,61 @@
 import tweepy
 import credentials
+import logging
+import pyowm
 from time import sleep
 from datetime import datetime, timezone
 from pytz import timezone
 
+emoji = {
+    'partly_cloudy' : u'\U000026C5',
+}
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
 est = timezone('US/Eastern')
 
-
+# Set credentials for Authentication
 consumer_key = credentials.env['consumer_key']
 consumer_secret = credentials.env['consumer_secret']
 access_token = credentials.env['access_token']
 access_token_secret = credentials.env['access_token_secret']
+owm_key = credentials.env['owm_key']
 
+# Tweepy setup
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+# OpenWeatherMaps setup
+owm = pyowm.OWM(owm_key)
+observation = owm.weather_manager().weather_at_place('48150')
+data = observation.weather
+print(data.status)
+print(data.temperature('fahrenheit'))
+
+map = \
+'.   ' + emoji['partly_cloudy'] + '\n' +\
+'  ' + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + '\n' +\
+'   ' + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + '\n' +\
+\
+\
+'     ' + emoji['partly_cloudy'] + '\n' +\
+'                    ' + emoji['partly_cloudy'] + emoji['partly_cloudy'] + '\n' +\
+'              ' + ' ' + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + '\n' +\
+'              ' + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + '\n' +\
+'             ' + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + '  ' + emoji['partly_cloudy'] + '\n' +\
+'             ' + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + '\n' +\
+'              ' + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + '\n' +\
+'              ' + ' ' + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + '\n' +\
+'              ' + ' ' + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + '\n' +\
+'              ' + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + emoji['partly_cloudy'] + '\n'\
+
+print(map)
+
 while True:
-    new_tweet = 'Timestamp: ' + str(datetime.now(est))
-    api.update_status(status=new_tweet)
+    #new_tweet = emoji['partly_cloudy'] + str(datetime.now(est))
+    logger.info('Tweeting now...')
+    api.update_status(status=map)
     sleep(900) # 15 minutes
