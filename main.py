005c9867d9aaa5f85b20452cb2f219d541379554
Jake Schreuder <jakeanton@gmail.com>
2021-03-21 17:35:08 -0400

    Functional enough to tweet a weather emoji map. Addded get_map function to take in the list of emojis. Added the ZIP code list of locations.

--- main.py
+++ main.py
@@ -6,6 +6,23 @@ from time import sleep
 from datetime import datetime, timezone
 from pytz import timezone
 
+zip_codes =  \
+[
+'49913',                                     \
+'49968', '49946', '49855', '49839', '49783',         \
+'49927', '49815', '49878', '49838', '49781', '49781',  \
+'49887',                                     \
+'49720', '49779',                              \
+'49684', '49735', '49746', '49707',                   \
+'49660', '49601', '48661', '48730',                   \
+'49459', '49307', '48858', '48706', '48731',          \
+'49445', '49343', '48847', '48655', '48461', '48401', \
+'49417', '49503', '48860', '48867', '48503', '48060', \
+'49423', '49325', '48910', '48350', '48048',          \
+'49043', '49008', '49269', '48104', '48226',          \
+'49128', '49091', '49242', '49221', '48162'           \
+]
+
 emoji = {
     'partly_cloudy' : u'\U000026C5',
     'mostly_sunny' : u'\U0001F324',
@@ -18,10 +35,15 @@ emoji = {
     'fog' : u'\U0001F32B',
 }
 
-def get_weather(zip):
-    observation = owm.weather_manager().weather_at_place(zip)
-    data = observation.weather
-    return data
+def get_weather(zip_code):
+    try:
+        observation = owm.weather_manager().weather_at_place(zip_code)
+        data = observation.weather
+        return data
+
+    except:
+        print(f'Error for {zip_code}')
+
 
 def assign_emoji(data):
     if (data.status == 'Thunderstorm'):
@@ -45,9 +67,27 @@ def assign_emoji(data):
         return emoji['tornado']
     else: #Sand, Dust, Ash, Squall, Mist, Haze
         return emoji['fog']
-    
-def get_emoji(zip):
-    return assign_emoji(get_weather(zip))
+
+
+# Takes in list of 55 emoji (mapped to each ZIP code), return Michigan
+def get_map(in_list):
+    map = \
+    '.   ' + in_list[0] + '\n' +\
+    '  ' + in_list[1] + in_list[2] + in_list[3] + in_list[4] + in_list[5] + '\n' +\
+    '   ' + in_list[6] + in_list[7] + in_list[8] + in_list[9] + in_list[10] + in_list[11] + '\n' +\
+    \
+    \
+    '     ' + in_list[12] + '\n' +\
+    '                    ' + in_list[13] + in_list[14] + '\n' +\
+    '              ' + ' ' + in_list[15] + in_list[16] + in_list[17] + in_list[18] + '\n' +\
+    '              ' + in_list[19] + in_list[20] + in_list[21] + in_list[22] + '\n' +\
+    '             ' + in_list[23] + in_list[24] + in_list[25] + in_list[26] + '  ' + in_list[27] + '\n' +\
+    '             ' + in_list[28] + in_list[29] + in_list[30] + in_list[31] + in_list[32] + in_list[33] + '\n' +\
+    '              ' + in_list[34] + in_list[35] + in_list[36] + in_list[37] + in_list[38] + in_list[39] + '\n' +\
+    '              ' + ' ' + in_list[40] + in_list[41] + in_list[42] + in_list[43] + in_list[44] + '\n' +\
+    '              ' + ' ' + in_list[45] + in_list[46] + in_list[47] + in_list[48] + in_list[49] + '\n' +\
+    '              ' + in_list[50] + in_list[51] + in_list[52] + in_list[53] + in_list[54] + '\n'
+    return map
 
 if __name__ == '__main__':
 
@@ -72,23 +112,11 @@ if __name__ == '__main__':
     # OpenWeatherMaps setup
     owm = pyowm.OWM(owm_key)
 
-    map = \
-    '.   ' + emoji['sunny'] + '\n' +\
-    '  ' + emoji['sunny'] + emoji['sunny'] + emoji['sunny'] + emoji['sunny'] + emoji['sunny'] + '\n' +\
-    '   ' + emoji['sunny'] + emoji['sunny'] + emoji['sunny'] + emoji['sunny'] + emoji['sunny'] + emoji['sunny'] + '\n' +\
-    \
-    \
-    '     ' + emoji['snow'] + '\n' +\
-    '                    ' + emoji['snow'] + emoji['snow'] + '\n' +\
-    '              ' + ' ' + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + '\n' +\
-    '              ' + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + '\n' +\
-    '             ' + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + '  ' + emoji['snow'] + '\n' +\
-    '             ' + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + '\n' +\
-    '              ' + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + '\n' +\
-    '              ' + ' ' + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + '\n' +\
-    '              ' + ' ' + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + '\n' +\
-    '              ' + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + emoji['snow'] + '\n'\
+    emoji_list = []
+    for zip_code in zip_codes:
+        emoji_list.append(assign_emoji(get_weather(zip_code)))
 
+    map = get_map(emoji_list)
     print(map)
 
     while True:
